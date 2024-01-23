24692490b32216fd2e2a5c7d5ebba75db5785458
kingno3l <nuelluiz@gmail.com>
2024-01-23 16:20:37 +0100

    navigation menu done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'foods/index'
+  get 'public_recipes/index'
+  get 'recipes/index'
+  get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,4 +11,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  root 'home#index' # Adjust the root path according to your setup
+
+  get '/recipes', to: 'recipes#index', as: 'recipes'
+  get '/public_recipes', to: 'public_recipes#index', as: 'public_recipes'
+  get '/foods', to: 'foods#index', as: 'foods'
+
 end
