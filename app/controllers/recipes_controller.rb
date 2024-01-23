3136dd2c2c014ede7b9483c6b21e00b67da5a048
BHObserver <BHObserver0@gmail.com>
2024-01-23 11:00:43 +0600

    Update recipes_controller.rb

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,5 @@
 class RecipesController < ApplicationController
-
+  before_action :authenticate_user!, except: %i[public_recipes]
 
   def index
     @recipes = current_user.recipes.includes({ recipe_foods: :food }, :user).all
@@ -13,7 +13,13 @@ class RecipesController < ApplicationController
     @public_recipes = Recipe.includes({ recipe_foods: :food }, :user).public_recipes
   end
 
+  def update_public_status
+    @recipe = current_user.recipes.find(params[:id])
+    authorize! :update, @recipe
+    @recipe.update(public: !@recipe.public)
 
+    redirect_to @recipe, notice: @recipe.public ? 'Recipe is now public.' : 'Recipe is now private.'
+  end
 
   def new
     @recipe = current_user.recipes.new
@@ -21,7 +27,7 @@ class RecipesController < ApplicationController
 
   def create
     @recipe = current_user.recipes.new(recipe_params)
-
+    authorize! :create, @recipe
 
     if @recipe.save
       redirect_to @recipe, notice: 'Recipe was successfully created.'
@@ -30,6 +36,14 @@ class RecipesController < ApplicationController
     end
   end
 
+  def destroy
+    @recipe = current_user.recipes.find(params[:id])
+    authorize! :destroy, @recipe
+
+    @recipe.destroy
+
+    redirect_to recipes_path, notice: 'Recipe was successfully destroyed.'
+  end
 
   private
 
