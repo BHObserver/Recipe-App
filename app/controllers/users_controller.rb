afacbf88c1dcb8574064da56a37096204f1f88f9
BHObserver <BHObserver0@gmail.com>
2024-01-25 00:20:33 +0600

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,13 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def show
+    @user = User.find(params[:id])
+    return unless @user.nil?
+
+    flash[:notice] = 'Signed out successfully.'
+    redirect_to root_path
+  end
+
 end
