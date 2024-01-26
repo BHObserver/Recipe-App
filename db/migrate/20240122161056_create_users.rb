fb98d6203180928288742ebff7faaaad7e4e0be8
BHObserver <BHObserver0@gmail.com>
2024-01-26 14:30:42 +0600

    update migration files

--- db/migrate/20240122161056_create_users.rb
+++ db/migrate/20240122161056_create_users.rb
@@ -1,4 +1,4 @@
-class CreateUsersTable < ActiveRecord::Migration[7.1]
+class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       t.string :name, null: false
